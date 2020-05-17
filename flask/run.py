086c7102e25a9b4bc59b3d96e29dc58a97b89ec0
Ayotunde Akinsola <ayotundeakinsola@gmail.com>
2020-05-17 01:12:32 +0000

    added if statement inside html

--- flask/run.py
+++ flask/run.py
@@ -1,6 +1,8 @@
 # provides a way of using operating system dependent functionality
 import os
 
+import json
+
 #import Flask class
 from flask import Flask, render_template
 
@@ -15,17 +17,21 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route('/careers')
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
+
 
 #This is an internal environment variable that cloud9 has set and we're using the os module from the standard library to get that environment variable for us. 
 if __name__ == '__main__':
